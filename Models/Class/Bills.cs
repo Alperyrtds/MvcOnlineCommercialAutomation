e49db563e0e0eab9a7def75d7b125621ebb7713b
Alper Yurtdaş <73263073+Alperyrtds@users.noreply.github.com>
2021-08-30 02:02:08 +0300

    Initial commit

---
+++ Models/Class/Bills.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineCommercialAutomation.Models.Class
+{
+    public class Bills
+    {
+        [Key]
+        public int BillId { get; set; }
+        public string BillSerialNo { get; set; }
+        public string BillRowNo { get; set; }
+        public DateTime Date { get; set; }
+        public string TaxAdministration { get; set; }
+        public DateTime Hour { get; set; }
+        public string Submitter { get; set; }
+        public string Receiver { get; set; }
+    }
+}
---
+++ Models/Class/Cariler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineCommercialAutomation.Models.Class
+{
+    public class Cariler
+    {
+        [Key]
+        public int CariId { get; set; }
+        public string CariName { get; set; } 
+        public string CariSurname { get; set; }  
+        public string CariCity { get; set; } 
+        public string CariMail { get; set; } 
+    }
+}
