9599fd17df9b7391f36dafc0fdc8bb096af1f0a8
Alper Yurtdaş <73263073+Alperyrtds@users.noreply.github.com>
2021-09-19 04:56:09 +0300

    Last State

---
+++ MvcOnlineCommercialAutomation/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using MvcOnlineCommercialAutomation.Models.Class;
+using MvcOnlineCommercialAutomation.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineCommercialAutomation.Controllers
+{
+    public class CategoryController : Controller
+    {
+        Context context = new Context();
+        // GET: Category
+        public ActionResult Index()
+        {
+           var p = context.TblCategories.ToList();
+            return View(p);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+          
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            context.TblCategories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var p = context.TblCategories.Find(id);
+            context.TblCategories.Remove(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult GetCategory(int id)
+        {
+            var p = context.TblCategories.Find(id);
+            return View("AddCategory", p);
+        }
+        [HttpPost]
+        public ActionResult GetCategory(Category category)
+        {
+            context.TblCategories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
